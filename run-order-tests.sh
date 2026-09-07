38c3406c4c6efd850dd656cf0fe28f655e97c516
ckcky <ckcky@icloud.com>
2026-09-08 00:00:28 +0800

    chore(023): 移除误入的本地测试辅助脚本 run-order-tests.sh

--- run-order-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-export JAVA_HOME=/Users/feizhai/Library/Java/JavaVirtualMachines/openjdk-26.0.1/Contents/Home
-export PATH="$JAVA_HOME/bin:$PATH"
-/Users/feizhai/TranStation/Payment/mvnw -q -pl order-service -am test 2>&1 | tail -40
-echo "MVN_EXIT=$?"
